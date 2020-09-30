562b430218fed54d3be2e216cf5191ed35cd2a5f
Mikhail Rantovich <mrantovich@gmail.com>
2020-09-30 22:46:16 +0700

    Секция works с адаптивом

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
 
-	
+	$('.works__item').mouseenter(function(){
+		$(this).find('span').addClass('_works-item-hovered');
+		$(this).find('.works__link-container').addClass('_works-item-hovered');
+	});
+	$('.works__item').mouseleave(function(){
+		$(this).find('span').removeClass('_works-item-hovered');
+		$(this).find('.works__link-container').removeClass('_works-item-hovered');
+	})
 
 });
