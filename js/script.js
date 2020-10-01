15ce90470021a6b6f88915e5d739f1ee25df730d
Mikhail Rantovich <mrantovich@gmail.com>
2020-10-01 22:30:38 +0700

    Адаптив для more + бургер

--- js/script.js
+++ js/script.js
@@ -7,6 +7,11 @@ $(document).ready(function(){
 	$('.works__item').mouseleave(function(){
 		$(this).find('span').removeClass('_works-item-hovered');
 		$(this).find('.works__link-container').removeClass('_works-item-hovered');
-	})
+	});
+
+	$('.burger').click(function(){
+		$(this).toggleClass('_active');
+		$('.header__nav').toggleClass('_active');
+	});
 
 });
