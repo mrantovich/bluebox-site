1f88fd494ff74c507bd0a0350f1a5f3c29181d02
Mikhail Rantovich <mrantovich@gmail.com>
2020-10-02 22:20:07 +0700

    Завёрстаны подвал и слайдер + адаптив

--- js/script.js
+++ js/script.js
@@ -15,6 +15,37 @@ $(document).ready(function(){
 	});
 
 	$('.clients__slider').slick({
+		dots: false,
+		infinite: true,
+		speed: 300,
+		slidesToShow: 4,
+		slidesToScroll: 4,
+		autoplay: true,
+		autoplaySpeed: 5000,
+		responsive: [
+			{
+				breakpoint: 1024,
+				settings: {
+				slidesToShow: 3,
+				slidesToScroll: 3,
+				infinite: true,
+				}
+			},
+			{
+				breakpoint: 876,
+				settings: {
+				slidesToShow: 2,
+				slidesToScroll: 2
+				}
+			},
+			{
+				breakpoint: 625,
+				settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1
+				}
+			}
+			]
 	});
 
 });
